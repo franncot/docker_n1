2aeeae51c4ad9ab687c81bb1776fc4508b9cddbb
franncot <franco.toq@gmail.com>
2023-12-14 11:21:53 -0600

    created only 1 security rule and modified to avoid the issue of limit for SG

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow-ssh-from-anywhere"
-  description = "Allow SSH inbound "
+resource "aws_security_group" "allow_full_stack" {
+  name        = "allow-fullstack-ports"
+  description = "Inbound for ssh, backend api, frontend, mongo express and access to http and https for docker install and repo. "
 
   ingress {
     from_port   = 22
@@ -22,65 +22,38 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_security_group" "allow_https_outbound" {
-  name        = "allow-https-outbound"
-  description = "Allow outbound HTTPS traffic"
-
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic to any destination on port 443
-  }
-}
-
-resource "aws_security_group" "allow_http_outbound" {
-  name        = "allow-http-outbound"
-  description = "Allow outbound HTTP traffic"
-
-  egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic to any destination on port 80
-  }
-}
-
-resource "aws_security_group" "allow_http_inbound_backend" {
-  name        = "allow-http-inbound-backend-from-anywhere"
-  description = "Allow http inbound for backend port 5000"
-
   ingress {
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_security_group" "allow_http_inbound_frontend" {
-  name        = "allow-http-inbound-frontend-from-anywhere"
-  description = "Allow http inbound for frontend port 3000"
-
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_security_group" "allow_http_inbound_mongoexpress" {
-  name        = "allow-http-inbound-mongoexpress-from-anywhere"
-  description = "Allow http inbound for Mongo express port 27017"
-
   ingress {
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "fullstack-SG"
   }
 }
 
@@ -89,7 +62,7 @@ resource "aws_instance" "fullStack" {
     instance_type = "t2.micro"
     key_name = "fullstack"
     user_data = file("deployment.sh")
-    security_groups = [aws_security_group.allow_ssh.name, aws_security_group.allow_https_outbound.name, aws_security_group.allow_http_outbound.name, aws_security_group.allow_http_inbound_backend.name, aws_security_group.allow_http_inbound_frontend.name, aws_security_group.allow_http_inbound_mongoexpress.name]
+    security_groups = [aws_security_group.allow_Full_Stack.name]
     tags = {
     Name  = "fullStackDocker"
   }
